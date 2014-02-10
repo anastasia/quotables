8e046a584bfc0da434b94f69d721002f773bebef
lennylemony <anastasia.aizman@gmail.com>
2014-02-10 14:32:17 -0800

    modulus not working,trying to figure out an alternative

--- public/popup.js
+++ public/popup.js
@@ -1,4 +1,70 @@
-// listen to background window
-// getSelection
+var app = angular.module('popupApp', ['ngRoute'])
+  .config(function($routeProvider){
+    $routeProvider
+      .when('/', {
+        controller: 'popupController',
+        // template: '<li>hi there</li>' // template goes here
+        // or templateUrl, with location, write your html there
+        templateUrl : 'popup.html' 
+      })
+    })
+  .controller('popupController', function ($scope, $http){
+  $http.get('/')
+    .success(function(data){
+      console.log(data);
+    })
+    .error(function(data){
+      console.log('Error: ' + data);
+    });
+// quote object
+  $scope.quoteObject = {
+    'title' : 'title',
+    'body' : 'body',
+    'author' : 'author',
+    'tags' : [],
+    'date' : 'date'
+  };
 
+// save quote
+  $scope.saveQuote = function(){
+    quoteObject.title = $('#quoteTitle').val();
+    quoteObject.body = $('#quoteBody').val();
+    quoteObject.author = $('#quoteAuthor').val();
+    quoteObject.tags = $('#quoteTags').val(); // split, make into an array
+    quoteObject.date = new Date();
+    console.log(quoteObject);
+  };
 
+});
+
+function indexController ($scope, $http){
+
+}
+  // $scope.createTodo = function() {
+  //   $http.post('/api/todos', $scope.formData)
+  //     .success(function(data){
+  //       $scope.formData = {}; // clear formdata
+  //       $scope.todos = data;
+  //       console.log(data);
+  //     })   
+  //     .error(function(data){
+  //       console.log('Error: '+ data);
+  //     });
+  // };
+
+// GET
+
+// POST
+// var postData = function(){
+//   jQuery.ajax({
+//       type: "POST",
+//       url: "popup.html",
+//       data: quoteObject,
+//       success: function(data) {
+//           console.log(data);
+//       }, 
+//       error: function(data){
+//         console.log("error ", data)
+//       }
+//   }); 
+// }
---
+++ server.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+
+// using https://modulus.io/ database
+mongoose.connect('mongodb://<fred>:<fred>@novus.modulusmongo.net:27017/dOs5ymym') // write localhost, 
+
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.logger('dev')); // log every request to the console // bodyParser creates a ton of temp files, change this later?
+  app.use(express.bodyParser()); // pull information from html in POST
+  app.use(express.methodOverride()); // simulate DELETE and PUT
+});
+
+// create mongoose model to perform all VERBS on 
+var Quotable = mongoose.model('Quotable', {
+  text : String // just want the text for each To do — mongo automatically generates _id for each
+});
+
+// MONGO URI
+// mongodb://<user>:<pass>@novus.modulusmongo.net:27017/dOs5ymym
+// MONGO CONSOLE
+// mongo novus.modulusmongo.net:27017/dOs5ymym -u <user> -p <pass>
+
+// GET
+app.get('/api/quotes/', function(req, res){
+  Quotable.find(function(err, quotes){
+    if (err){
+      res.send(err)
+    }
+    res.json(quotes);
+  });
+});
+
+// POST
+app.post('/', function(req, res){
+  Quotable.create({
+    text : req.body.text,
+    done : false ////////////////////////////////////////// what's this?
+  }, function(err, todo){
+    if(err){
+      res.send(err);
+    }
+    // get all, return newly created
+    Quotable.find(function(err, quotes){
+      if(err){
+        res.send(err);
+      }
+      res.json(quotes);
+    });
+  });
+});
+
+// DELETE
+// app.delete('', function(req, res){
+//   Quotable.remove({
+//     _id : req.params.popup_id
+//   }, function(err, todo){ // err, data
+//     if(err){
+//       res.json(err);
+//     }
+
+//     Quotable.find(function(err, quotes){
+//       if(err){
+//         res.send(err);
+//       }
+//       res.json(quotes)
+//     });
+//   });
+// });
+
+// load popup.html when we go to localhost:8080
+app.get('*', function(req, res){
+  res.sendfile('./public/popup.html')
+})
+
+app.listen(8080);
+console.log("App listening, port 8080");
