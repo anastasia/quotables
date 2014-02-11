eb372b7acdadd6459f95d72913a652b9880d782d
lennylemony <anastasia.aizman@gmail.com>
2014-02-11 00:19:11 -0800

    some styling

--- public/background.js
+++ public/background.js
@@ -9,7 +9,8 @@ $(function() {
     document.getElementById('quoteDate').innerHTML = d;
     console.log(d)
     // var text = tab.getSelection().baseNode
-  })
+  });
+
   chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
   }, function(selection) {
@@ -20,14 +21,14 @@ $(function() {
     }
   });
 
-  chrome.extension.onRequest.addListener(function(request, sender, sendResponse{
-    if (request.method === 'saveQuote')
-      sendResponse({result: db.getGetUserList()});
-    // else if (request.method === 'GetUser')
-      // sendResponse({result: db.getGetUser(request.username)});
-    else
-      sendResponse({}); // snub them.
-    })
+  chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
+    // if (request.method === 'saveQuote')
+    //   // sendResponse({result: db.getGetUserList()});
+    // // else if (request.method === 'GetUser')
+    //   // sendResponse({result: db.getGetUser(request.username)});
+    // else
+    //   console.log("nooo"); // snub them.
+    // })
 
-  // });
+  });
 });
