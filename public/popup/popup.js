db6ab551bbcfb02477bdb5dda0d0f756d60859f0
lennylemony <anastasia.aizman@gmail.com>
2014-02-11 12:07:28 -0800

    got rid of popup's content scripts

---
+++ public/popup/popup.js
@@ -0,0 +1,60 @@
+var app = angular.module('popupApp', ['ngRoute', 'firebase'])// 
+  .config(function($routeProvider){
+    $routeProvider
+      .when('/', {
+        controller: 'PopupController',
+        templateUrl : 'popup.html' 
+      })
+      // .when('/index.html', {
+      //   controller: 'QuoteController',
+      //   templateUrl : 'index.html' 
+      // })
+    })
+  .controller('PopupController', function ($scope, $firebase){
+    $scope.quoteObject = {
+      'title' : 'title', 
+      'body' : 'body',
+      'author' : 'author',
+      'tags' : [],
+      'date' : 'date'
+    };
+    // first save quote, then post
+    $scope.sendQuote = function(data){
+      console.log("sending quote!", data)
+      var quotations = new Firebase("https://quotable.firebaseio.com");
+      $scope.quote = $firebase(quotations);
+      $scope.quote.$add(data);
+    };
+    
+    // close popup after saving
+    $scope.closeWindow = function(){
+      window.close();
+    };
+
+    $scope.tagSeparator = function(tags){
+      $scope.tagArr = [];
+      tagArr = tags.replace(/,/g, '').split(' ').sort();
+    };
+    // save quote
+    $scope.saveQuote = function(){
+      $scope.tagSeparator($('#quoteTags').val());
+      
+      $scope.quoteObject.title = title;
+      $scope.quoteObject.body = highlighted;
+      $scope.quoteObject.author = $('#quoteAuthor').val();
+      $scope.quoteObject.tags = tagArr; // split, make into an array
+      $scope.quoteObject.date = new Date();
+      $scope.quoteObject.url = url;
+      // URL HERE
+      console.log("saving quote: ", $scope.quoteObject);
+      $scope.sendQuote($scope.quoteObject);
+    };
+  })
+  // .controller('QuoteController', function ($scope, $firebase){
+  //   $scope.quotes = new Firebase('https://quotable.firebaseio.com');
+  //   $scope.quotes.on('value', function(all) {
+  //     console.log(all.val())
+  //     $scope.allQuotes = all.val();
+  //   });
+  // });
+
