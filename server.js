ee2c865d7757ff26b708462dd5ed9fffdf33dfe9
lennylemony <anastasia.aizman@gmail.com>
2014-04-06 22:06:20 -0700

    testing sqlite3

--- server.js
+++ server.js
@@ -1,90 +1,64 @@
-var mysql      = require('mysql');
 var express    = require('express');
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password'
+var fs         = require("fs");
+var file       = "test.db";
+var exists     = fs.existsSync(file);
+var sqlite3    = require("sqlite3").verbose();
+var db         = new sqlite3.Database(file);
+var app        = express();
+
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
 });
 
-connection.connect();
-var app = express();
-
-app.set('port', 3000);
-
-app.configure(function(){
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.logger('dev')); // log every request to the console // bodyParser creates a ton of temp files, change this later?
-  app.use(express.bodyParser()); // pull information from html in POST
-  app.use(express.methodOverride()); // simulate DELETE and PUT
-});
-
-// connection.query('USE test_database');
-// app.get('/api', function (req, res) {
-//   res.send('Our Sample API is up...');
-// });
 
 
-app.get('/', function(req, res){
-  connection.query('SELECT * FROM users', function(err, rows){
-    res.render('users', {users : rows});
+db.serialize(function(){
+
+
+
+
+})
+
+app.get("/dynamic", function(req, res) {
+    var db = new sqlite3.Database(test)
+    var posts = []
+
+    db.serialize(function() {
+        db.each("SELECT * FROM test", function(err, row) {
+            posts.push(row)
+        })
+    }, function(){
+      res.render("dynamic", {title: "Dynamic", posts: posts})
+      
+    })
+
+})
+
+
+db.serialize(function() {
+  if(!exists) {
+    db.run("CREATE TABLE Stuff (thing TEXT)");
+  }
+
+  var stmt = db.prepare("INSERT INTO Stuff VALUES (?)");
+
+  //Insert random data
+  var rnd;
+  for (var i = 0; i < 10; i++) {
+    rnd = Math.floor(Math.random() * 10000000);
+    stmt.run("Thing #" + rnd);
+  }
+
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, thing FROM Stuff", function(err, row) {
+    console.log(row.id + ": " + row.thing);
   });
 });
 
 
-// app.get('/user/:id', function (req, res){
-//         connection.connect();
-//         connection.query('SELECT * FROM users where id ='+req.params.id,
-//             function (error, rows, fields) {
-//                       res.writeHead(200, {'Content-Type': 'text/plain'});
-//                   		if(!rows.length) {
-//                     		res.end( 'no such record found...');
-//                     		//break;
-//                   		} else {
-//                     		var ans = 'User is '+ rows[0].username +'\n';
-//                     		res.end(ans);
-//                   		}});
-//       connection.end();
-// });
-
-
-// connection.query('CREATE DATABASE IF NOT EXISTS quotables', function (err) {
-//     if (err) throw err;
-//     connection.query('USE quotables', function (err) {
-//         if (err) throw err;
-//         connection.query('CREATE TABLE IF NOT EXISTS users('
-//             + 'id INT NOT NULL AUTO_INCREMENT,'
-//             + 'PRIMARY KEY(id),'
-//             + 'name VARCHAR(100)'
-//             +  ')', function (err) {
-//                 if (err) throw err;
-//             });
-//     });
-
-//     connection.query('USE quotables', function (err) {
-//         if (err) throw err;
-//         connection.query('CREATE TABLE IF NOT EXISTS quotes('
-//             + 'id INT NOT NULL AUTO_INCREMENT,'
-//             + 'PRIMARY KEY(id),'
-//             + 'body VARCHAR(1000) NOT NULL,'
-//             + 'author VARCHAR(100),'
-//             + 'created TIMESTAMP DEFAULT 0000-00-00 00:00:00,'
-//             + 'updated TIMESTAMP DEFAULT NOW() on update NOW()'
-//             +  ')', function (err) {
-//                 if (err) throw err;
-//             });
-//     });
-
-    // connection.query('USE quotables', function (err) {
-    //     if (err) throw err;
-    //     connection.query('CREATE TABLE IF NOT EXISTS tags('
-    //         + 'id INT NOT NULL AUTO_INCREMENT,'
-    //         + 'PRIMARY KEY(id),'
-    //         + 'name VARCHAR(100)'
-    //         +  ')', function (err) {
-    //             if (err) throw err;
-    //         });
-    // });
-
-// });
-app.listen(app.get('port'));
-console.log('Express server listening on port ' + app.get('port'));
+// db.close();
