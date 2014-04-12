b36de5739c2e3f5d712f6475140b9b37e7a5a38d
lennylemony <anastasia.aizman@gmail.com>
2014-04-12 13:16:44 -0700

    basic table structure

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs      = require("fs");
-var file    = process.env.CLOUD_DIR + "/" + "test.db";
+var file    = "test.db";
 var exists  = fs.existsSync(file);
 var app     = express();
 if(!exists) {
@@ -13,25 +13,11 @@ var db = new sqlite3.Database(file);
 
 db.serialize(function() {
   if(!exists) {
-    db.run("CREATE TABLE Users (user_id INTEGER UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(20))");
-    db.run("CREATE TABLE Quotes (quote_id INTEGER UNIQUE NOT NULL PRIMARY KEY, content VARCHAR, author VARCHAR, title VARCHAR, url VARCHAR)");
-    db.run("CREATE TABLE Tags (tag_id INTEGER UNIQUE NOT NULL PRIMARY KEY, tag VARCHAR(20), user_id)");
-    db.run("CREATE TABLE Quotes_Tags (tag_id, quote_id)");
+    db.run("create table if not exists users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(100))");
+    db.run("create table if not exists quotes (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT(1000), url TEXT(1000), author TEXT(100), title TEXT(200))");
+    db.run("create table if not exists tags (id INTEGER PRIMARY KEY AUTOINCREMENT, tag TEXT(200))");
+    db.run("create table if not exists quotes_tags (quotes INTEGER, tags INTEGER)");
   }
-
-  var stmt = db.prepare("INSERT INTO Users VALUES (?)");
-
-  //Insert random data
-  var rnd;
-  for (var i = 0; i &lt; 10; i++) {
-    rnd = Math.floor(Math.random() * 10000000);
-    stmt.run("Thing #" + rnd);
-  }
-
-  stmt.finalize();
-  db.each("SELECT rowid AS id, thing FROM Stuff", function(err, row) {
-    console.log(row.id + ": " + row.thing);
-  });
 });
 
 db.close();
@@ -53,66 +39,12 @@ app.configure(function(){
 
 
 app.get('/', function(req, res){
-  connection.query('SELECT * FROM users', function(err, rows){
-    res.render('users', {users : rows});
+  connection.query('SELECT * FROM Quotes', function(err, rows){
+    res.render('content', {content : rows});
   });
 });
 
 
-// app.get('/user/:id', function (req, res){
-//         connection.connect();
-//         connection.query('SELECT * FROM users where id ='+req.params.id,
-//             function (error, rows, fields) {
-//                       res.writeHead(200, {'Content-Type': 'text/plain'});
-//                   		if(!rows.length) {
-//                     		res.end( 'no such record found...');
-//                     		//break;
-//                   		} else {
-//                     		var ans = 'User is '+ rows[0].username +'\n';
-//                     		res.end(ans);
-//                   		}});
-//       connection.end();
-// });
 
-
-// connection.query('CREATE DATABASE IF NOT EXISTS quotables', function (err) {
-//     if (err) throw err;
-//     connection.query('USE quotables', function (err) {
-//         if (err) throw err;
-//         connection.query('CREATE TABLE IF NOT EXISTS users('
-//             + 'id INT NOT NULL AUTO_INCREMENT,'
-//             + 'PRIMARY KEY(id),'
-//             + 'name VARCHAR(100)'
-//             +  ')', function (err) {
-//                 if (err) throw err;
-//             });
-//     });
-
-//     connection.query('USE quotables', function (err) {
-//         if (err) throw err;
-//         connection.query('CREATE TABLE IF NOT EXISTS quotes('
-//             + 'id INT NOT NULL AUTO_INCREMENT,'
-//             + 'PRIMARY KEY(id),'
-//             + 'body VARCHAR(1000) NOT NULL,'
-//             + 'author VARCHAR(100),'
-//             + 'created TIMESTAMP DEFAULT 0000-00-00 00:00:00,'
-//             + 'updated TIMESTAMP DEFAULT NOW() on update NOW()'
-//             +  ')', function (err) {
-//                 if (err) throw err;
-//             });
-//     });
-
-    // connection.query('USE quotables', function (err) {
-    //     if (err) throw err;
-    //     connection.query('CREATE TABLE IF NOT EXISTS tags('
-    //         + 'id INT NOT NULL AUTO_INCREMENT,'
-    //         + 'PRIMARY KEY(id),'
-    //         + 'name VARCHAR(100)'
-    //         +  ')', function (err) {
-    //             if (err) throw err;
-    //         });
-    // });
-
-// });
 app.listen(app.get('port'));
 console.log('Express server listening on port ' + app.get('port'));
