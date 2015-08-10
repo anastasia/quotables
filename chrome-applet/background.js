55be43fb0e4d01aa0447019e1eee4b8bbf516377
Anastasia Aizman <anastasia.aizman@gmail.com>
2015-08-09 19:13:45 -0700

    add - chrome extension, saving quote to server

--- chrome-applet/background.js
+++ chrome-applet/background.js
@@ -1,45 +1,7 @@
-
-var highlighted, url, title;
-$(function() {
-  chrome.tabs.getSelected(null, function(tab) {
-    url = tab.url;
-    title = tab.title;
-    d = new Date();
-
-    document.getElementById('quoteTitle').value = title;
-    document.getElementById('quoteDate').innerHTML = d;
-
-
-    // var text = tab.getSelection().baseNode
+function getPageDetails(callback) {
+  chrome.tabs.executeScript(null, { file: 'getselection.js' });
+  chrome.runtime.onMessage.addListener(function(message)  {
+    console.log("getting message?", message);
+    callback(message);
   });
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-      //code in here will run every time a user goes onto a new tab, so you can insert your scripts into every new tab
-  });
-
-  // chrome.tabs.executeScript( {
-  //   code: "window.getSelection().toString();"
-  //   }, function(selection) {
-  //   if(selection){
-  //     highlighted = selection[0];
-  //     document.getElementById('quoteBody').value = highlighted;
-  //     console.log(highlighted)
-  //   }
-  // });
-  chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
-
-  });
-
-  //
-  // chrome.app.runtime.onLaunched.addListener(function() {
-  //   chrome.app.window.create('verser.html', {
-  //     'outerBounds': {
-  //       'width': 400,
-  //       'height': 500
-  //     }
-  //   });
-  // });
-
-
-
-
-});
+};
