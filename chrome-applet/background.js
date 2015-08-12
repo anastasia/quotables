534edc34571c9239ade6c3a866be21b010b6b2dd
Anastasia Aizman <anastasia.aizman@gmail.com>
2015-08-11 21:14:50 -0700

    remove - console.logs

--- chrome-applet/background.js
+++ chrome-applet/background.js
@@ -1,7 +1,6 @@
 function getPageDetails(callback) {
   chrome.tabs.executeScript(null, { file: 'getselection.js' });
   chrome.runtime.onMessage.addListener(function(message)  {
-    console.log("getting message?", message);
     callback(message);
   });
 };
